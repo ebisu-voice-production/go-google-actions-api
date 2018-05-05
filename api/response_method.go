edd7d37569d36e59856fbd419c53894db7a963eb
daishi <daishi@axlight.com>
2018-05-05 23:18:11 +0900

    tell card

--- api/response_method.go
+++ api/response_method.go
@@ -80,8 +80,57 @@ func (res *AppResponse) AskSsml(ssml string) *AppResponse {
 	return res
 }
 
+func (res *AppResponse) TellCard(title string, formatted string) *AppResponse {
+	res.FinalResponse = &FinalResponse{
+		RichResponse: &RichResponse{
+			Items: []Item{
+				{
+					BasicCard: &BasicCard{
+						Title:         title,
+						FormattedText: formatted,
+					},
+				},
+			},
+		},
+	}
+	return res
+}
+
+func (res *AppResponse) AttachCardButton(title string, url string) *AppResponse {
+	if res.FinalResponse != nil &&
+		res.FinalResponse.RichResponse != nil &&
+		len(res.FinalResponse.RichResponse.Items) >= 1 &&
+		res.FinalResponse.RichResponse.Items[0].BasicCard != nil {
+		res.FinalResponse.RichResponse.Items[0].BasicCard.Buttons = []Button{
+			{
+				Title: title,
+				OpenUrlAction: &OpenUrlAction{
+					Url: url,
+				},
+			},
+		}
+	}
+	if len(res.ExpectedInputs) > 0 &&
+		res.ExpectedInputs[0].InputPrompt != nil &&
+		res.ExpectedInputs[0].InputPrompt.RichInitialPrompt != nil &&
+		len(res.ExpectedInputs[0].InputPrompt.RichInitialPrompt.Items) >= 1 &&
+		res.ExpectedInputs[0].InputPrompt.RichInitialPrompt.Items[0].BasicCard != nil {
+		res.ExpectedInputs[0].InputPrompt.RichInitialPrompt.Items[0].BasicCard.Buttons = []Button{
+
+			{
+				Title: title,
+				OpenUrlAction: &OpenUrlAction{
+					Url: url,
+				},
+			},
+		}
+	}
+	return res
+}
+
 func (res *AppResponse) AttachLinkOut(title string, url string) *AppResponse {
-	if res.FinalResponse != nil && res.FinalResponse.RichResponse != nil {
+	if res.FinalResponse != nil &&
+		res.FinalResponse.RichResponse != nil {
 		res.FinalResponse.RichResponse.LinkOutSuggestion = &LinkOutSuggestion{
 			DestinationName: title,
 			OpenUrlAction: &OpenUrlAction{
@@ -89,7 +138,9 @@ func (res *AppResponse) AttachLinkOut(title string, url string) *AppResponse {
 			},
 		}
 	}
-	if len(res.ExpectedInputs) > 0 && res.ExpectedInputs[0].InputPrompt != nil && res.ExpectedInputs[0].InputPrompt.RichInitialPrompt != nil {
+	if len(res.ExpectedInputs) > 0 &&
+		res.ExpectedInputs[0].InputPrompt != nil &&
+		res.ExpectedInputs[0].InputPrompt.RichInitialPrompt != nil {
 		res.ExpectedInputs[0].InputPrompt.RichInitialPrompt.LinkOutSuggestion = &LinkOutSuggestion{
 			DestinationName: title,
 			OpenUrlAction: &OpenUrlAction{
